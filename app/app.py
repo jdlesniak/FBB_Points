f11751d4065cf6287d3acdd6c3a9d8b5b481652d
jdlesniak <johndlesniak@gmail.com>
2025-03-15 08:52:53 -0400

    UPDATE to APP and NEW calcPoints

--- app/app.py
+++ app/app.py
@@ -1,10 +1,20 @@
+import os
+import requests
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import streamlit.components.v1 as components
 #import matplotlib.pyplot as plt
 
+from calcPoints import *
+
 def main():
+    local = False
+    nTeams = 14
+
+    fbb = query_data(local)
+    
     st.title("Fantasy Baseball Points Projections")
     st.write("""This app uses ZiPS projections and a bespoke fit blown saves model to project player points based on
               your league's scoring settings.""")
@@ -35,7 +45,7 @@ def main():
 
     if st.button("Submit"):
         values_array = np.array(batter_values + pitcher_values)
-        df = pd.DataFrame([values_array])
+        calculate_points(values_array, nTeams, local)
         st.write("### Captured Values Table:")
         st.dataframe(df)
 
---
+++ app/calcPoints.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import requests
+
+from dotenv import load_dotenv
+import pandas as pd
+import numpy as np
+import streamlit as st
+    
+def query_data(local):
+
+    ## get params to access data
+    if local:
+        API = os.getenv("GS_API_KEY")
+        FBB_SHEET_ID = os.getenv("FBB_SHEET_ID")
+        FBB_RANGE = os.getenv("FBB_RANGE")
+    else:
+        API = st.secrets["GS_API_KEY"]
+        FBB_SHEET_ID = st.secrets["FBB_SHEET_ID"]
+        FBB_RANGE = st.secrets["FBB_RANGE"]
+
+        # API Endpoint
+    URL = f"https://sheets.googleapis.com/v4/spreadsheets/{FBB_SHEET_ID}/values/{FBB_RANGE}?key={API}"
+
+    # Fetch Data
+    response = requests.get(URL)
+    data = response.json()
+
+    # Convert to Pandas DataFrame
+    if "values" in data:
+        values = data["values"]
+        # First row as column names
+        fbb = pd.DataFrame(values[1:], columns=values[0])
+
+    return fbb  
+
+def calculate_points(values_array, nTeams, local):
+    ## get the data
+    fbb = query_data(local)
+    
+    ## hold out key columns for a return df
+    output = fbb[['Name','Team','POS']]
+
+    ## grab the values as a matrix and multiply to get point values
+    projMat = fbb.drop(['Name','Team','POS','PlayerId'], axis = 1).values
+    points = np.matmul(projMat, values_array)
+    output['points'] = points
+
+    ## sort and reset the index
+    output.sort_values(by = 'Points', ascending = False, inplace = True)
+    output.reset_index(drop = True, inplace = True)
+
+    ## add in rank and round
+    output['rank'] = output.index + 1
+    output['Projected Round'] = np.ceil(output['rank']/nTeams)
+
+    return output
+
+
+    print('this will calculate points based on user provided inputs, retuns df')
