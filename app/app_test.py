3c76972a7cd49ef4a855f61ab05238949cacb79d
jdlesniak <johndlesniak@gmail.com>
2025-03-15 15:18:36 -0400

    NEW: test file to debug issues querying data

---
+++ app/app_test.py
@@ -0,0 +1,62 @@
+import os
+import requests
+import sys
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit.components.v1 as components
+#import matplotlib.pyplot as plt
+sys.path.append('/Users/John/Documents/allProjects/FBB_Points/app/')
+
+from calcPoints import *
+
+def main():
+    local = os.path.isfile('/Users/John/Documents/allProjects/data_hidden/FBB_Points/clean/allClean.csv')
+    nTeams = 14
+    
+    st.title("Fantasy Baseball Points Projections")
+    st.write("This app uses ZiPS projections to project player points based on your league's scoring settings and number of teams.")
+    
+    # Custom styling to change the width of number input
+    st.markdown("""
+        <style>
+            div[data-testid="stNumberInput"] input {
+                width: 100px !important;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+    st.write("Enter Number of Teams")
+    tcols = st.columns(10)
+    nTeams = tcols[0].number_input("Number of Teams:", min_value = 1, max_value = 30, value=12, step = 1)
+    st.write("Input Batter Values")
+    # Creating 10 number input fields in a single row
+    bcols = st.columns(10)
+    batter_labels = ["1B", "2B", "3B", "HR", "R", "RBI", "BB", "SB", "CS", "HBP"]
+    batter_defaults = [1,2,3,4,1,2,.5,1,-1,.5]
+    batter_values = [bcols[i].number_input(f"{batter_labels[i]}:", min_value=-5.0, max_value=10.0,
+                     value=float(batter_defaults[i]), step=0.5, format="%.1f") for i in range(10)]
+
+    st.write("Input Pitcher Values")
+    # Creating 8 number input fields in a single row
+    pcols = st.columns(10)
+    pitcher_labels = ["W", "IP", "HLD", "SV", "K", "ER", "BS"]
+    pitcher_defaults = [8,1,2,4,1,-1,-2]
+    pitcher_values = [pcols[i].number_input(f"{pitcher_labels[i]}:", min_value=-5.0, max_value=10.0,
+                      value=float(pitcher_defaults[i]), step=0.5, format="%.1f") for i in range(7)]
+
+    if st.button("Submit"):
+        ## append the values together
+        values_array = np.array(batter_values + pitcher_values)
+        
+        ## calcualte the points
+        df = query_data(local)
+        
+        ## log the df in the session_state
+        st.session_state.df = df
+        ## write the dataframe with points to the app
+        st.write("### ZiPS Powered Projections:")
+        
+
+if __name__ == "__main__":
+    main()
